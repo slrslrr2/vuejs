7dd0cebaac22045d22147ea41bf491ae10490849
slrslrr2 <slrslrr1@gmail.com>
2022-09-08 17:41:47 +0900

    2. 라우터 & 컴포넌트 설계 

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <header>
+        <router-link to="/login">로그인</router-link> |
+        <router-link to="/signup">회원가입</router-link>
+      </header>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
