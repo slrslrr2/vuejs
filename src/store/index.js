0c7eb49873bb8ee87ea01b843e295cb30654db71
slrslrr2 <slrslrr1@gmail.com>
2022-09-09 15:23:11 +0900

    로그인 상태 관리와 스토어

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    username: '',
+  },
+  getters: {
+    // 해당 값의 변화를 감지한다(computed같은것)
+    isLogin(state) {
+      return state.username !== '';
+    },
+  },
+  mutations: {
+    setUsername(state, username) {
+      state.username = username;
+    },
+    clearUsername(state) {
+      state.username = '';
+    },
+  },
+});
