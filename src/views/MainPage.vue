e071f64fcf579265243721097fa8c0ba8e664618
slrslrr2 <slrslrr1@gmail.com>
2022-09-11 00:09:49 +0900

    브라우저 저장소를 이용한 인증값 관리 style

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,14 +2,13 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
-      <LoadingSpinner v-if="isLoading">></LoadingSpinner>
+      <LoadingSpinner v-if="isLoading"></LoadingSpinner>
       <ul v-else>
         <PostListItem
           v-for="postItem in postItems"
           :key="postItem._id"
           :postItem="postItem"
-        >
-        </PostListItem>
+        ></PostListItem>
       </ul>
     </div>
   </div>
@@ -19,6 +18,7 @@
 import PostListItem from '@/components/posts/PostListItem.vue';
 import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
 import { fetchPosts } from '@/api/index';
+
 export default {
   components: {
     PostListItem,
@@ -32,40 +32,14 @@ export default {
   },
   methods: {
     async fetchData() {
-      console.log('isLoading : true');
       this.isLoading = true;
       const { data } = await fetchPosts();
       this.isLoading = false;
-      console.log('isLoading : false');
-
       this.postItems = data.posts;
     },
   },
-  beforeCreate() {
-    console.log('beforeCreate');
-  },
   created() {
-    console.log('created1');
     this.fetchData();
-    console.log('created2');
-  },
-  beforeMount() {
-    console.log('beforeMount');
-  },
-  mounted() {
-    console.log('mounted');
-  },
-  beforeUpdate() {
-    console.log('beforeUpdate');
-  },
-  updated() {
-    console.log('updated');
-  },
-  beforeDestroy() {
-    console.log('beforeDestroy');
-  },
-  destroyed() {
-    console.log('destroyed');
   },
 };
 </script>
